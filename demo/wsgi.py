5d17eccf389cd847dbaf806ead16b57de1cceef9
Alexander Lyon <arlyon@me.com>
2017-02-19 23:22:59 +0100

    static files

--- demo/wsgi.py
+++ demo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for demo project.
+WSGI config for arlyon-demo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,9 +8,27 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-
+import time
+import traceback
+import signal
+import sys
 from django.core.wsgi import get_wsgi_application
+import site
 
+site.addsitedir('/home/django/arlyon-demo/venv/lib/python3.5/site-packages')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo.settings")
+
+
+
+try:
+    application = get_wsgi_application()
+    print('WSGI without exception')
+except Exception:
+    print('handling WSGI exception')
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
 
 application = get_wsgi_application()
